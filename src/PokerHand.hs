b27c1b21befb3a48e67714cbc9807ad4d5c563c3
Suxue LI <suxue.li@octo.com>
2021-11-26 16:40:56 +0100

    retirer le test sur le royal flush de 7 cartes

--- src/PokerHand.hs
+++ src/PokerHand.hs
@@ -12,7 +12,7 @@ extraireLaCouleurDUneCarte carte = carte!!1
 extraireLaListeDesCouleurs :: [String] -> [Char]
 extraireLaListeDesCouleurs main = map extraireLaCouleurDUneCarte main
 
-aUneRoyalFlush :: [String] -> Bool
-aUneRoyalFlush main =
+aUneStraightFlush :: [String] -> Bool
+aUneStraightFlush main =
     length main >= 5 &&
     all (==extraireLaCouleurDUneCarte (main!!0)) (extraireLaListeDesCouleurs main)
--- test/PokerHandSpec.hs
+++ test/PokerHandSpec.hs
@@ -8,29 +8,18 @@ spec = do
         it "2 est plus petite que 3" $ do
             "2s" `estPlusPetite` "3s"  `shouldBe` True
 
-    describe "aUneRoyalFlush" $ do
+    describe "aUneStraightFlush" $ do
         it "retourne faux si la main a moins de 5 cartes" $ do
-            aUneRoyalFlush ["1s", "2s", "3s", "4s"] `shouldBe` False
+            aUneStraightFlush ["1s", "2s", "3s", "4s"] `shouldBe` False
 
         it "retourne vrai si la main correspond à 1s,2s,3s,4s,5s" $ do
-            aUneRoyalFlush ["1s", "2s", "3s", "4s", "5s"] `shouldBe` True
+            aUneStraightFlush ["1s", "2s", "3s", "4s", "5s"] `shouldBe` True
 
         it "retourne vrai si la main correspond à 6s,7s,8s,9s,Ts" $ do
-            aUneRoyalFlush ["6s", "7s", "8s", "9s", "Ts"] `shouldBe` True
+            aUneStraightFlush ["6s", "7s", "8s", "9s", "Ts"] `shouldBe` True
 
         it "retourne vrai si la main correspond à 6h,7h,8h,9h,Th" $ do
-            aUneRoyalFlush ["6h", "7h", "8h", "9h", "Th"] `shouldBe` True
+            aUneStraightFlush ["6h", "7h", "8h", "9h", "Th"] `shouldBe` True
 
         it "retourne vrai si la main a plus de 5 cartes de la même couleur" $ do
-            aUneRoyalFlush ["6h", "7h", "8h", "9h", "Th", "Jh", "Qh"] `shouldBe` True
-
-        it "retourne vrai si la main a une royal flush et d'autres cartes d'une couleur différente" $ do
-            aUneRoyalFlush ["6h", "7h", "8h", "9h", "Th", "Js", "Qs"] `shouldBe` True
-
-
---        it "retourne faux si la dernière carte de la main n'est pas de la même couleur" $ do
---            aUneRoyalFlush ["1s", "2s", "3s", "4s", "5c"] `shouldBe` False
---
---        it "retourne faux si toutes les cartes ne sont pas de la même couleur" $ do
---            aUneRoyalFlush ["1s", "2s", "3s", "4c", "5s"] `shouldBe` False
-
+            aUneStraightFlush ["6h", "7h", "8h", "9h", "Th", "Jh", "Qh"] `shouldBe` True
