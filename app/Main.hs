64e7356f68e37bdc248539fe9bce8bd4337cdda3
Quentin Cattez <qcattez@octo.com>
2022-02-25 16:57:14 +0100

    bootstrap du programme principal

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,16 @@
 module Main where
+import PokerHand
+
+litEntree:: IO [String]
+litEntree = do
+    ligne <- getLine
+    if ligne == ""
+        then return []
+        else do
+            suivantes <- litEntree
+            return $ ligne:suivantes
 
 main :: IO ()
-main = undefined
+main = do
+    entree <- litEntree
+    print entree
